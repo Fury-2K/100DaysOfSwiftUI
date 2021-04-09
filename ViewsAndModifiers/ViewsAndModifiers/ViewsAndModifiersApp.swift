f905cfefd8a3cecf03611f3c467aad0720f37287
Manas Aggarwal <manas.agrawal990@gmail.com>
2021-04-09 20:32:54 +0530

    Add ViewAndModifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Manas Aggarwal on 08/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
