85406d45efd78cf2be72d189af8cf6de19447bd3
Manas Aggarwal <manas.agrawal990@gmail.com>
2021-04-13 12:00:01 +0530

    Add betterRest app

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Manas Aggarwal on 13/04/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
