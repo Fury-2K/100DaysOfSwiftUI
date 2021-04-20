629cc6d22d2e7c7261389827b4a5ac92a90ce11b
Manas Aggarwal <manas.agrawal990@gmail.com>
2021-04-20 12:27:58 +0530

    Add app

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Manas Aggarwal on 20/04/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
