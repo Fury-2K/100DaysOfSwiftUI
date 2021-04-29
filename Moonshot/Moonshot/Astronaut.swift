922e4dfcff9ae071e169944e3c7ca200019d47bc
Manas Aggarwal <manas.agrawal990@gmail.com>
2021-04-29 16:16:16 +0530

    Add app

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Manas Aggarwal on 29/04/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    var id: String
+    var name: String
+    var description: String
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Manas Aggarwal on 28/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
