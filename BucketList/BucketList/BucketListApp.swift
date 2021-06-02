9c7235f3ee792568af90c752b4bb18bf3f98add9
Manas Aggarwal <manas.agrawal990@gmail.com>
2021-06-02 12:29:17 +0530

    add app

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Manas Aggarwal on 29/05/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
